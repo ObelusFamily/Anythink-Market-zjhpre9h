5827503afb4b7cb1058df90947e8edfca66387af
WilcoApp <hello@wilco.gg>
2023-03-01 07:38:04 +0000

    Fixing A Bug

--- backend/app/api/routes/items/items_resource.py
+++ backend/app/api/routes/items/items_resource.py
@@ -25,11 +25,6 @@ from app.resources import strings
 from app.services.items import check_item_exists, get_slug_for_item
 from app.services.event import send_event
 
-import os
-import openai
-
-openai.api_key = os.getenv('OPENAI_API_KEY')
-
 router = APIRouter()
 
 
@@ -73,13 +68,6 @@ async def create_new_item(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=strings.ITEM_ALREADY_EXISTS,
         )
-    if not item_create.image:
-        response = openai.Image.create(
-            prompt=item_create.title,
-            n=1,
-            size='256x256'
-        )
-        item_create.image = response['data'][0]['url']
     item = await items_repo.create_item(
         slug=slug,
         title=item_create.title,
